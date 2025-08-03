4d1045001c763e048b590bbc2474da05de534aa3
mukulkatewa <2023kuec2079@iiitkota.ac.in>
2025-08-03 22:53:27 +0530

    PeerJs server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = require("http").createServer(app);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/myapp"
+});
+
+app.use("/peerjs", peerServer);
+
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => {
+  console.log(`✅ PeerJS server running on http://localhost:${PORT}`);
+});
